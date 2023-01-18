cbfef0cad5a16c2362b5a5f476b9ca6a31217350
talhaunas1 <mtalha4600@gmail.com>
2023-01-18 21:47:06 +0500

    abc

--- index.js
+++ index.js
@@ -12,13 +12,19 @@ app.use(cookieParser());
 app.use(express.json());
 dotenv.config();
 
-const corsOptions = {
-  origin: "http://localhost:3001", 
-  credentials: true,            //access-control-allow-credentials:true
-  optionSuccessStatus: 200
-}
+// const corsOptions = {
+//   origin: "http://localhost:3001", 
+//   credentials: true,            //access-control-allow-credentials:true
+//   optionSuccessStatus: 200
+// }
 
-app.use(cors(corsOptions));
+// app.use(cors(corsOptions));
+
+app.use(cors({
+    credentials: true,
+    origin: "http://localhost:3001/",
+    // origin: "http://localhost:3000",
+}));
 
 const port = 5000;
 
